e54e50ab853fc9410885efd1c42fd910d9b7b981
Ugoo <ugonnaezeabara@gmail.com>
2022-10-10 08:55:47 -0700

    function sorts an array of integers in ascending order using Bubble sort and file contains its time complexity

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ *
+ * @array: Array contains the elements in an unordered way
+ * @size: Size of the array
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, a;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap */
+				dup = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = dup;
+				print_array(array, size);
+			}
+		}
+	}
+}
