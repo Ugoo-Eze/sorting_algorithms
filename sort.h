76c67448523d9052c9476dffed63000e5837c2d9
Ugoo <ugonnaezeabara@gmail.com>
2022-10-10 10:30:55 -0700

    made change

--- sort.h
+++ sort.h
@@ -29,7 +29,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 /* Helper Functions Prototypes */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 
 #endif
